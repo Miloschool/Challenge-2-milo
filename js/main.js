9bfff03351dd608334014ae2c26469d20c7f227a
Miloschool <19077165@student.hhs.nl>
2021-03-11 10:02:05 +0100

    aa

--- js/main.js
+++ js/main.js
@@ -15,43 +15,42 @@ function todayTime() {
     m = checkTime(m); // Tijden apart gemaakt voor checking time om 0 toe te voegen bij tijden met alleen 1 cijfer
     s = checkTime(s); // Tijden apart gemaakt voor checking time om 0 toe te voegen bij tijden met alleen 1 cijfer
 
+    var currentTime;
+    if (screen.width <= 425) { // Time placeholder width
+        currentTime = h + ':' + m;
+    } else {
+        currentTime = h + ':' + m + ':' + s;
+    }
+     document.getElementById('timeShow').innerHTML = currentTime;
 
-    //  INPUT HER DROPDOWN OPTION FOR TIMEZINE
-  
-    var currentTime = h + ':' + m + ':' + s;
-    
-  
-    document.getElementById('timeShow').innerHTML = currentTime;
+
 
     var swapForPlaceholder = document.getElementById('swapImagePlaceholder');
     var visualDateImg = document.getElementById('dayCyclus');
 
-       switch (true) { //  
-        case s >= 0&& s <= 14:
+    switch (true) { // check time
+        case s >= 0 && s <= 14:
             swapForPlaceholder.classList.add('switchImage');
             visualDateImg.src = 'images/Morning.svg';
             break;
         case s >= 16 && s <= 29:
             swapForPlaceholder.classList.add('switchImage');
-            visualDateImg.src =  'images/Day.svg';
+            visualDateImg.src = 'images/Day.svg';
             break;
         case s >= 31 && s <= 44:
             swapForPlaceholder.classList.add('switchImage');
-            visualDateImg.src =  'images/Evening.svg';
+            visualDateImg.src = 'images/Evening.svg';
             break;
-        case  s >= 46&& s <= 60:
+        case s >= 46 && s <= 58:
             swapForPlaceholder.classList.add('switchImage');
-            visualDateImg.src =  'images/Night.svg';
+            visualDateImg.src = 'images/Night.svg';
             break;
         default:
             swapForPlaceholder.classList.remove('switchImage');
             break;
-
-    }   
+    }
     setTimeout(todayTime, 500);
 }
-
-
 
 todayTime();
 
@@ -68,6 +67,7 @@ function todayDate() {
 }
 
 todayDate();
+
 
 function checkTime(i) {
     if (i < 10) {
